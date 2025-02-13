a6efb76e69a657c853492cbacede701435b6a538
Dorian <s232808@dtu.dk>
2025-02-13 21:34:17 +0100

    upload current status (13.Feb.2025) of homework

---
+++ preclass_assignment/functions.py
@@ -0,0 +1,16 @@
+def say_hello(name):
+    """Return a greeting to the user."""
+    return f"Hello {name}!"
+
+
+def square(num):
+    """Return the square of a number."""
+    return num ** 2
+
+def sqrt(num):
+    """Return the square root of a number."""
+    return num ** 0.5
+
+def cube(num):
+    """Return the cube of a number."""
+    return num ** 3
